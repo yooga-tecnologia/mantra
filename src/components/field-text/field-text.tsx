1c8c6b2e5140d826edd1401c074ec1fc0b8a2036
Ana Có <ana.co@yooga.com.br>
2026-07-22 16:06:03 -0300

    fix(field-text): remove empty elements

--- src/components/field-text/field-text.tsx
+++ src/components/field-text/field-text.tsx
@@ -392,24 +392,26 @@ export class FieldText {
   render() {
     return (
       <Host class={this.fieldTextClass}>
-        <div class={`${this.componentPrefix}-label`}>
-          {renderFieldLabel({
-            labelText: this.labelText,
-            prefix: this.componentPrefix,
-            labelId: this.name + '-label',
-            required: this.host.hasAttribute('required'),
-          })}
-          {this.hasInfoButton && (
-            <div class={`${this.componentPrefix}-info-button`}>
-              <slot name="info-button"></slot>
-            </div>
-          )}
-          {this.hasActionButton && (
-            <div class={`${this.componentPrefix}-action-button`}>
-              <slot name="action-button"></slot>
-            </div>
-          )}
-        </div>
+        {this.labelText && (
+          <div class={`${this.componentPrefix}-label`}>
+            {renderFieldLabel({
+              labelText: this.labelText,
+              prefix: this.componentPrefix,
+              labelId: this.name + '-label',
+              required: this.host.hasAttribute('required'),
+            })}
+            {this.hasInfoButton && (
+              <div class={`${this.componentPrefix}-info-button`}>
+                <slot name="info-button"></slot>
+              </div>
+            )}
+            {this.hasActionButton && (
+              <div class={`${this.componentPrefix}-action-button`}>
+                <slot name="action-button"></slot>
+              </div>
+            )}
+          </div>
+        )}
         {this.renderInput()}
         {this.renderInlineMessage()}
       </Host>
